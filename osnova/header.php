0b64c8ea6c00de0cd2fc59359d470b3af6eddc07
yummiqu <154753013+yummiqu@users.noreply.github.com>
2024-11-28 19:25:56 +0400

    Add files via upload

---
+++ osnova/header.php
@@ -0,0 +1,7 @@
+<link rel="stylesheet" href="style.css">
+
+    <header class="headerglav">
+        <h1>Сделаем вместе<br> лучше!</h1>
+        <p>Прием заявок на устранение проблем в городе</p>
+    </header>
+
---
+++ osnova/header1.php
@@ -0,0 +1,13 @@
+<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+<link rel="stylesheet" href="style.css">
+
+        <div class="block_yellow"></div>
+        <div class="header_kab">
+        <h1>Сделаем вместе<br> лучше!</h1>
+        <div class="nav_kab">
+                <li><a href="glavnaya/main1.php" onclick="$('main').load('glavnaya/main1.php');">Главная</a></li>
+                <li><a onclick="$('main').load('templates/zayvki.php');">Мои заявки</a></li>
+                <li><a onclick="$('main').load('templates/create_zayvka.php');">Создать заявку</a></li>
+        </div>
+        </div>
+       
